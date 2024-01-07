9744550e5c1febe8c40103074278e525df179dc6
m3lky <robinjut10000@gmail.com>
2024-01-07 06:03:00 -0400

    update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import "./Home.scss";
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import Products from "../Products/Products";
-import {products} from '../../data/products'
+import { products } from "../../data/products";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner />
-            <div className="main-content">
-                <div className="layout">
-                    <Category />
-                    <Products
-                        headingText="Trending"
-                        products={products}
-                    />
-                </div>
-            </div>
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://server-tech-store.onrender.com/upsever"
+        );
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <Banner />
+      <div className="main-content">
+        <div className="layout">
+          <Category />
+          <Products headingText="Trending" products={products} />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
